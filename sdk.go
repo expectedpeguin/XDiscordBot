add44962a28ce214cda538025f87dbb4743d2ffd
ExpectedPeguin <124583215+expectedpeguin@users.noreply.github.com>
2024-08-12 04:07:09 +0200

    Update sdk.go

--- sdk.go
+++ sdk.go
@@ -1,6 +1,8 @@
 package sdk
+import "github.com/bwmarrin/discordgo"
 
 type BotCore interface {
+	SendEmbedMessage(channel string, message *discordgo.MessageEmbed, button ...discordgo.Button) error
 	SendMessage(channel string, message string) error
 	Log(message string)
 	SomeMainFunction() string
